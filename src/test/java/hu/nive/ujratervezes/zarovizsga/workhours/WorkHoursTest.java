7fbe75568a23dda074abbf205697e32ec06eabbf
lippaitamas1021 <lippaitamas1021@gmail.com>
2021-03-16 19:51:39 +0100

    WorkHours commit #2

--- src/test/java/hu/nive/ujratervezes/zarovizsga/workhours/WorkHoursTest.java
+++ src/test/java/hu/nive/ujratervezes/zarovizsga/workhours/WorkHoursTest.java
@@ -8,7 +8,7 @@ public class WorkHoursTest {
     @Test
     void minWork() {
         WorkHours workHours = new WorkHours();
-        String result = workHours.minWork("src/main/resources/hu/nive/ujratervezes/zarovizsga/workhours/workhours.txt");
+        String result = workHours.minWork("src/test/resources/hu/nive/ujratervezes/zarovizsga/workhours/workhours.txt");
         assertEquals("John Doe: 2021-01-04", result);
     }
 }
